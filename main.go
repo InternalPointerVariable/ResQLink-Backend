7aae12a5ace3c0ce33e38c50f1331155763a8ba5
xGihyun <giordnnuz@gmail.com>
2025-03-02 10:19:21 +0800

    Initial server setup

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic("Failed to load .env file.")
+	}
+
+	dbUrl, ok := os.LookupEnv("DATABASE_URL")
+	if !ok {
+		panic("DATABASE_URL not found.")
+	}
+
+	ctx := context.Background()
+
+	pool, err := pgxpool.New(ctx, dbUrl)
+	defer pool.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", health)
+
+	host, ok := os.LookupEnv("HOST")
+	if !ok {
+		panic("HOST not found.")
+	}
+
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		panic("PORT not found.")
+	}
+
+	server := http.Server{
+		Addr: host + ":" + port,
+	}
+
+	slog.Info(fmt.Sprintf("Starting server on port: %s", port))
+
+	server.ListenAndServe()
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	slog.Info("Hello, World!")
+}
